01066226b73299e2cac2885ca21f89f0eda09093
Juan Francisco Herrera <egarigaribay@gmail.com>
2017-11-04 12:30:03 -0600

    learn recursion

---
+++ factorial.py
@@ -0,0 +1,18 @@
+# ~*~ coding: utf-8
+
+##
+# 
+##
+
+def factorial(number):
+	if number == 0:
+		return 1
+	else:
+		return number * factorial(number - 1)
+
+if __name__ == '__main__':
+	number = int(raw_input('Escribe un número: '))
+
+	result = factorial(number)
+
+	print ('{:,}').format(result)
