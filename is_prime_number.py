4b019f247d73f9e68811055ec2a2f53abf0b79c3
Juan Francisco Herrera <egarigaribay@gmail.com>
2017-11-03 22:41:17 -0600

    validate its a prime number

---
+++ is_prime_number.py
@@ -0,0 +1,32 @@
+# ~*~ coding: utf-8
+
+###########################################################################################################################
+# En matemáticas, un número primo es un número mayor que 1 que tiene únicamente dos divisores distintos: él mismo y el 1. #
+###########################################################################################################################
+
+def is_prime(number):
+	if number > 2:
+		for x in xrange(2,number):
+			if number % x != 0:
+				if number % number == 0 and number % 1 == 0:
+					response = True
+			else:
+				response = False
+				break
+
+	return response
+
+def start():
+	digit = raw_input('Introduce un número: ')
+
+	if digit.isdigit() == False and digit == 'exit':
+		exit()
+	elif is_prime(int(digit)) == True:
+		print('El numero {}, es primo'.format(digit))
+	else:
+		print('{}, No es primo'.format(digit))
+
+if __name__ == '__main__':
+	start()
+
+
