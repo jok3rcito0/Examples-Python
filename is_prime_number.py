9794645d1f22419777662de24ab3804bb80a24fa
Juan Francisco Herrera <egarigaribay@gmail.com>
2017-11-03 22:56:22 -0600

    add a loop so it doesn't close

--- is_prime_number.py
+++ is_prime_number.py
@@ -17,14 +17,17 @@ def is_prime(number):
 	return response
 
 def start():
-	digit = raw_input('Introduce un número: ')
-
-	if digit.isdigit() == False and digit == 'exit':
-		exit()
-	elif is_prime(int(digit)) == True:
-		print('El numero {}, es primo'.format(digit))
-	else:
-		print('{}, No es primo'.format(digit))
+	while True:
+		try:
+			digit = raw_input('Introduce un número: ')
+			if digit.isdigit() == False and digit == 'exit':
+				exit()
+			elif is_prime(int(digit)) == True:
+				print('El numero {}, es primo'.format(digit))
+			else:
+				print('{}, No es primo'.format(digit))
+		except ValueError:
+			print ('Si quieres salir escribe "exit" o escribe un entero y averigua si es primo')
 
 if __name__ == '__main__':
 	start()
